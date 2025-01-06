366974add62a10f2e6d81bbc27b52e7e2841d12d
mktoho <mk@mktoho.org>
2025-01-06 11:37:39 +0900

    OGPを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,30 @@ export const metadata: Metadata = {
   title: 'Xでログインしてツイートする',
   description:
     'OAuth2.0を使ってXにログインしてツイートする動作確認のページです。POST tweets APIの使用回数制限を確認したい。',
+  openGraph: {
+    title: 'Xでログインしてツイートする',
+    description:
+      'OAuth2.0を使ってXにログインしてツイートする動作確認のページです。POST tweets APIの使用回数制限を確認したい。',
+    url: 'https://tweet.mktoho.dev/',
+    siteName: 'Xでログインしてツイートするサイト',
+    images: [
+      {
+        url: 'https://tweet.mktoho.dev/ogp.png',
+        width: 1200,
+        height: 630,
+        alt: 'Xでログインしてツイートする',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Xでログインしてツイートする',
+    description:
+      'OAuth2.0を使ってXにログインしてツイートする動作確認のページです。POST tweets APIの使用回数制限を確認したい。',
+    images: 'https://tweet.mktoho.dev/ogp-twitter.png',
+  },
 }
 
 export default function RootLayout({
